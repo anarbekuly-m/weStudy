0329c4108f75e6e1700405300130b09e010ac8a0
meirzhan <meirzhan.anarbekuly@gmail.com>
2024-12-09 21:31:04 +0500

    come on baby

---
+++ src/main/java/mvc/westudy/WeStudyApplication.java
@@ -0,0 +1,13 @@
+package mvc.westudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mvc/westudy/WeStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package mvc.westudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
